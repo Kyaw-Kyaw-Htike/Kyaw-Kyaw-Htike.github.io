ba11fd1cd056325e5afcbc3ce1ab5020ca50eb5c
Dr. Kyaw Kyaw Htike @ Ali Abdul Ghafur <34255453+Kyaw-Kyaw-Htike@users.noreply.github.com>
2025-01-18 20:24:41 +0800

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-// after clicking an item in the the navbar menu icon, collapse it automatically so that user does not have to click it again to collapse it.
-$('.navbar-nav>li>a').on('click', function(){
-    $('.navbar-collapse').collapse('hide');
-});
